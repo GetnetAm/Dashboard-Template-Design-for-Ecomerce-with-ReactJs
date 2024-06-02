3d410a26d22165e9eb2ecc038c3be7a3fe88140a
GetnetAm <getnetamsalu2119@gmail.com>
2024-06-02 08:46:00 -0700

    header design

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,6 +5,7 @@ import Searchbar from "../searchbar/Searchbar";
 import Nav from "../nav/Nav";
 function Header() {
   return (
+    // heder design
     <div id="header" className="header fixed-top d-flex align-items-center">
       <Logo />
       <Searchbar />
